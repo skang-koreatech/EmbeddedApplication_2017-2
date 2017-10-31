566fdfc60a545a18d098152afdd9a195c08660ad
Seungwoo Kang <swkang@koreatech.ac.kr>
2017-10-31 13:06:03 +0900

    web server examples using Flask

---
+++ web_server/hello_flask.py
@@ -0,0 +1,10 @@
+from  flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return("hello raspberryp with flask!")
+
+if __name__ == "__main__":
+	app.run(host='192.168.0.13', port=8888, debug=True)
+
